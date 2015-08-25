11304122d5d23889ac7526c27258e71fca22bb65
Deak Zsolt <lordmairtis@gmail.com>
2015-08-25 22:20:20 +0200

    working link gathering from a single car search result page

--- downloadCarLinks.ps1
+++ downloadCarLinks.ps1
@@ -13,12 +13,18 @@ Param
     [String]
     $Uri
  ) 
+$scriptStartTime = (Get-Date)
+
+$outfile = '.\src\scrapedLinks.txt'
+If(Test-Path $outfile){
+    Remove-Item $outfile
+}
 
 $url = $Uri
 
-[Microsoft.PowerShell.Commands.HtmlWebResponseObject]$doc = Try { Invoke-WebRequest -Uri $url -Method Get <#-OutFile $Dest -PassThru -UseBasicParsing#> } Catch { $_.Exception.Response }
+[Microsoft.PowerShell.Commands.HtmlWebResponseObject]$doc = Try { Invoke-WebRequest -Uri $url -Method Get } Catch { $_.Exception.Response }
 #Response is OK, and yet to be html
-if(($doc.StatusCode -ne 200) -or !($doc.Headers['Content-Type'] -like '*text/html*') )#$IndexPage.Headers -contains 'text/html') )
+if(($doc.StatusCode -ne 200) -or !($doc.Headers['Content-Type'] -like '*text/html*') )
 {
     Throw "Inproducable server response" #exit
 }
@@ -26,7 +32,20 @@ if(($doc.StatusCode -ne 200) -or !($doc.Headers['Content-Type'] -like '*text/htm
  #Parse data
 Write-Host "Parsing data..."
 Try{
-    $elements = $doc.ParsedHtml.GetElementsByTagName("DIV") | where {$_.className -contains "talalati_lista_head"}
+    $elements = $doc.ParsedHtml.GetElementsByTagName("DIV") | Where-Object className -contains "talalati_lista_head"
 } Catch{
     Continue
 }
+
+Write-Host "Finding links..."
+[regex]$regExpression = "http.*`""
+ForEach($element in $elements){
+    $link = $regExpression.Match($element.innerHtml).Value
+    If($link){
+        Out-File $outfile -InputObject $link.Substring(0,$link.Length-1) -Append
+    }
+    $link = $null   
+}
+
+Write-Host "Done"
+Write-Host $(((Get-Date) - $scriptStartTime).totalseconds) 'seconds elapsed'
