b3baca14ffd5b0de0080d9249ed677b149ddaf7a
Deak Zsolt <lordmairtis@gmail.com>
2017-08-13 20:21:32 +0200

    test

--- tests/test_comparator_service.py
+++ tests/test_comparator_service.py
@@ -1,6 +1,7 @@
 import unittest
 from services.comparator_service import compare_cars
 from services.constants import POWER_KEY, WORTH_KEY, CAR_KEY, CONDITION_KEY
+from services.constants import TRUNK_KEY, MASS_KEY
 
 
 class TestHappyPaths(unittest.TestCase):
@@ -25,13 +26,33 @@ class TestHappyPaths(unittest.TestCase):
 	def test_power_worth(self):
 		car = self.defaultInput[0]
 		car[POWER_KEY] = '42 LE'
+
 		compare_cars(self.defaultInput)
+
 		self.assertEqual(car[WORTH_KEY], 3)
 
 	def test_condition_worth(self):
 		car1 = self.defaultInput[0]
 		car2 = self.defaultInput[1]
 		car2[CONDITION_KEY] = ''
+
 		compare_cars(self.defaultInput)
+
 		self.assertEqual(car1[WORTH_KEY], 0)
 		self.assertEqual(car2[WORTH_KEY], -20)
+
+	def test_trunk_worth(self):
+		car1 = self.defaultInput[0]
+		car1[TRUNK_KEY] = '290 l'
+
+		compare_cars(self.defaultInput)
+
+		self.assertEqual(car1[WORTH_KEY], 2)
+
+	def test_mass_worth(self):
+		car1 = self.defaultInput[0]
+		car1[MASS_KEY] = '1600 kg'
+
+		compare_cars(self.defaultInput)
+
+		self.assertEqual(car1[WORTH_KEY], 3)
