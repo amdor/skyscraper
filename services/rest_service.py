2dbe23ba32a0d0ed3a6ac621177c4a515810a834
Deak Zsolt <lordmairtis@gmail.com>
2017-11-21 22:00:32 +0100

    https communication comparing on rest call

--- services/rest_service.py
+++ services/rest_service.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, abort
+from flask import Flask, request, abort, Response
+from flask_restful import Resource, Api
 import re
+
+from services.comparator_service import CarComparator
 from services.scraper_service import ScraperService
 
 app = Flask(__name__)
@@ -16,12 +18,15 @@ class ScraperApi(Resource):
 		if request.is_json:
 			request_data = request.get_json()
 			if URL_KEY not in request_data:
-				abort(400, message="Provide car URLS")
+				abort(Response('Provide car URLS', status=400))
+				# abort(400, message="Provide car URLS")
 			for url in request_data[URL_KEY]:
 				if re.match(URl_PATTERN, url):
 					valid_urls.append(url)
 		scraper = ScraperService(valid_urls)
-		return scraper.get_car_data(), 200, {'Access-Control-Allow-Origin': '*'}
+		data = scraper.get_car_data()
+		CarComparator.compare_cars(data)
+		return data, 200, {'Access-Control-Allow-Origin': '*'}
 
 	def options(self):
 		return {'Allow': 'POST'}, 200, \
@@ -33,4 +38,5 @@ class ScraperApi(Resource):
 api.add_resource(ScraperApi, '/')
 
 if __name__ == '__main__':
-	app.run()
+	context = ('certificate.crt', 'privatekey.key')  # certificate and key files
+	app.run(ssl_context=context)
