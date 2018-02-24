11ee5416c2fb1e1a43d772d717e7a4cfd7a0812f
Zsolt <lordmairtis@gmail.com>
2018-02-24 15:17:05 +0100

    expanding html content functionality for mass prefetch

--- skyscraper/rest_service.py
+++ skyscraper/rest_service.py
@@ -3,14 +3,14 @@ from flask_restful import Resource, Api
 import re
 
 from skyscraper.comparator_service import CarComparator
-from skyscraper.scraper_service import ScraperService
+from skyscraper.scraper_service import ScraperService, ScraperServiceFactory
 
 app = Flask(__name__)
 api = Api(app)
 
 URl_PATTERN = '^http[s]?:\/\/www.hasznaltauto.hu\/auto\/([a-zA-Z]|\d|\/|_|-|\.)+$'
 URL_KEY = 'carUrls'
-HTML_KEY = 'html'
+HTML_KEY = 'htmls'
 
 
 class ScrapeByUrls(Resource):
@@ -20,13 +20,15 @@ class ScrapeByUrls(Resource):
 			request_data = request.get_json()
 			if URL_KEY not in request_data:
 				abort(Response('Provide car URL', status=400))
-			# abort(400, message="Provide car URLS")
 			for url in request_data[URL_KEY]:
 				if re.match(URl_PATTERN, url):
 					valid_urls.append(url)
-			html_content = request_data.get(HTML_KEY, '')
-			scraper = ScraperService(valid_urls)
-			data = scraper.get_car_data(html_content)
+			html_contents = request_data.get(HTML_KEY, {})
+			if html_contents:
+				scraper = ScraperServiceFactory.get_for_dict(html_contents)
+			else:
+				scraper = ScraperService(valid_urls)
+			data = scraper.get_car_data()
 			CarComparator.compare_cars(data)
 			return data, 200, {'Access-Control-Allow-Origin': '*'}
 		else:
--- skyscraper/scraper_service.py
+++ skyscraper/scraper_service.py
@@ -8,13 +8,17 @@ from skyscraper.comparator_service import CarComparator
 The scraper/html parser module for the hasznaltauto.hu's car detail pages.
 Usage:
 	- From console: run 'python scraper_service.py url1 url2 ...'
-	- As a Python class: initialize the class with an array of strings as parameter. The array must contain the URLs
+	- As a Python class: 
+		* Initialize the class with an array of strings as parameter. The array must contain the URLs
+		* Optionally use a dictionary called htmls, which must contain the car_urls as keys and the corresponding
+		html pages as values.
 """
 
 
 class ScraperService:
-	def __init__(self, car_urls=[]):
+	def __init__(self, car_urls=[], htmls = {}):
 		self.car_urls = car_urls
+		self.htmls = htmls
 
 	@staticmethod
 	def __table_to_dictionary(table_tag) -> dict:
@@ -47,23 +51,44 @@ class ScraperService:
 		table_dict = ScraperService.__table_to_dictionary(table)
 		return ScraperService.__build_car_data(table_dict, url)
 
-	def get_car_data(self, html = ''):
+	def get_car_data(self):
 		"""
-		Gets car data from the initialized urls
+		Gets car data from the urls the service was initialized with, or uses prefetched html data
 		:rtype: dict
 		:return: car data in dictionary. For keys see CAR_FEATURE_KEY_MAP's values
 		"""
 		car_data = []
-		if html:
-			car_soup = BeautifulSoup(html, 'lxml')
-			car_data.append(ScraperService.__parse_car(car_soup, self.car_urls[0]))
-			return car_data
 		headers = {'User-Agent': 'Chrome/60.0.3112.113'}
 		for car_url in self.car_urls:
 			response = requests.get(car_url, headers=headers)
 			car_soup = BeautifulSoup(response.content, 'lxml')
 			car_data.append(ScraperService.__parse_car(car_soup, car_url))
 		return car_data
+
+
+class ScraperServiceFactory:
+	def __init__(self):
+		pass
+
+	@staticmethod
+	def get_for_lists(url_list, content_list) -> ScraperService:
+		"""
+		Constructs a new Scraper sercice from two lists
+		:param url_list: list of urls
+		:param content_list: list of html pages in same order as in url list
+		:return: new ScraperService instance with the given content
+		"""
+		htmls = {url_list[i]:content_list[i] for i in range(len(url_list))}
+		return ScraperService(url_list, htmls)
+
+	@staticmethod
+	def get_for_dict(htmls: dict) -> ScraperService:
+		"""
+		Convenience method for ScraperService creation
+		:param htmls: dictionary where the keys are urls and the values are the html contents of the urls
+		:return: a ScraperService of the given data
+		"""
+		return ScraperService([*htmls], htmls)
 
 
 def main():
@@ -78,6 +103,7 @@ def main():
 	for car in data:
 		print(car['CarUri'])
 		print(car['worth'])
+
 
 if __name__ == "__main__":
 	main()
