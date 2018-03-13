5f1ff912dedb7e80178543720f4007058e59654a
Zsolt <lordmairtis@gmail.com>
2018-03-13 21:39:06 +0100

    parse speed tests

--- tests/parse_speed_test.py
+++ tests/parse_speed_test.py
@@ -22,16 +22,30 @@ class TestParseSpeed(unittest.TestCase):
 	def setUpClass(cls):
 		path = os.path.dirname(os.path.realpath(__file__))
 		cls.files_under_test = gather_extension_files(path)
-		for file_name in [*VALIDATION_DATA]:
-			abs_path = list(filter(lambda test_file: test_file.endswith(file_name), cls.files_under_test))[0]
-			with open(abs_path, 'rb') as html_file:
+		for path in cls.files_under_test:
+			with open(path, 'rb') as html_file:
 				content = html_file.read()
 				content = str(content, encoding='utf-8')
 				content = content.replace('\xa0', ' ')
 				cls.file_contents.append(content)
 
 	def test_lxml_parsing(self):
+		print('lxml parser')
 		start_time = time.clock()
 		for content in self.file_contents:
 			parse_html(content, 'lxml')
+		print(time.clock() - start_time, "seconds")
+
+	def test_html_parser_parsing(self):
+		print('html.parser')
+		start_time = time.clock()
+		for content in self.file_contents:
+			parse_html(content, 'html.parser')
+		print(time.clock() - start_time, "seconds")
+
+	def test_html5lib_parsing(self):
+		print('html5lib')
+		start_time = time.clock()
+		for content in self.file_contents:
+			parse_html(content, 'html5lib')
 		print(time.clock() - start_time, "seconds")
