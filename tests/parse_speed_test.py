6da045dcfa8c998fb3ea9a4856bc71414781be32
Deák, Zsolt <zsolt.deak@ge.com>
2018-03-13 16:51:33 +0100

    parse speed test 1

--- tests/parse_speed_test.py
+++ tests/parse_speed_test.py
@@ -1,17 +1,37 @@
-from os import listdir
+import unittest
+import os
+
+import re
+
+import time
 from bs4 import BeautifulSoup
-from skyscraper.scraper_service import ScraperService
-
-DIR_NAME = './resources/html/'
-
-def parse_html():
-	files = listdir(DIR_NAME)
-	car_data = []
-	for file in files:
-		with open(DIR_NAME + file) as fp:
-			car_soup = BeautifulSoup(fp, 'lxml')
-			car_data.append(ScraperService.parse_car(car_soup, file))
-	print(car_data)
+from .common_test_utils import gather_extension_files, VALIDATION_DATA
 
 
-parse_html()
+def parse_html(content, parser):
+	car_soup = BeautifulSoup(content, parser)
+	mileage = car_soup.find(text=re.compile('^((\d{1,3}[\.| |,]?){1,3})km|miles$'))
+	print(mileage)
+
+
+class TestParseSpeed(unittest.TestCase):
+	files_under_test = set()
+	file_contents = []
+
+	@classmethod
+	def setUpClass(cls):
+		path = os.path.dirname(os.path.realpath(__file__))
+		cls.files_under_test = gather_extension_files(path)
+		for file_name in [*VALIDATION_DATA]:
+			abs_path = list(filter(lambda test_file: test_file.endswith(file_name), cls.files_under_test))[0]
+			with open(abs_path, 'rb') as html_file:
+				content = html_file.read()
+				content = str(content, encoding='utf-8')
+				content = content.replace('\xa0', ' ')
+				cls.file_contents.append(content)
+
+	def test_lxml_parsing(self):
+		start_time = time.clock()
+		for content in self.file_contents:
+			parse_html(content, 'lxml')
+		print(time.clock() - start_time, "seconds")
--- tests/test_scraper_service.py
+++ tests/test_scraper_service.py
@@ -3,32 +3,9 @@ import unittest
 
 from skyscraper.scraper_service import ScraperServiceFactory
 from skyscraper.utils.constants import SPEEDOMETER_KEY
-
-VALIDATION_DATA = {
-	'bmw_530xd_automata-12119695.html': {
-		SPEEDOMETER_KEY: '98 100 km'
-	},
-	'audi_a4_avant.html': {
-		SPEEDOMETER_KEY: '56,488 km'
-	}
-}
+from .common_test_utils import gather_extension_files, VALIDATION_DATA
 
 
-def gather_extension_files(root):
-	"""
-	Traverses through all subdirectories of root collecting *.js filenames. Recursive.
-	:param root: the basepoint of search
-	:rtype: set
-	:return: all found js filenames
-	"""
-	extension_files = set()
-	for child in os.listdir(root):
-		abs_path = os.path.join(root, child)
-		if os.path.isdir(abs_path):
-			extension_files |= gather_extension_files(abs_path)
-		elif child.endswith(".html"):
-			extension_files.add(abs_path)
-	return extension_files
 
 
 class TestBasicPaths(unittest.TestCase):
