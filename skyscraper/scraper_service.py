28cf251220a2991cdea2841bdb904740cca2da50
Zsolt Deák <lordmairtis@gmail.com>
2019-02-24 17:11:42 +0100

    fix tests, fix age value algorithm

--- skyscraper/scraper_service.py
+++ skyscraper/scraper_service.py
@@ -21,154 +21,156 @@ Usage:
 
 
 class ScraperService:
-	def __init__(self, car_urls=[], htmls={}):
-		self.car_urls = car_urls
-		self.htmls = htmls
+    def __init__(self, car_urls=[], htmls={}):
+        self.car_urls = car_urls
+        self.htmls = htmls
 
-	@staticmethod
-	def __extract_result(search_result):
-		return '' if not search_result else search_result[0]
+    @staticmethod
+    def __extract_result(search_result):
+        return '' if not search_result else search_result[0]
 
-	@staticmethod
-	def __search_for_regex(expression, text, start_pos=0, end_pos=-1):
-		search_result = re.compile(expression).search(text, start_pos, end_pos)
-		# fallback
-		if not search_result and start_pos > 0:
-			return ScraperService.__search_for_regex(expression, text, 0, len(text)-1)
-		return ScraperService.__extract_result(search_result)
+    @staticmethod
+    def __search_for_regex(expression, text, start_pos=0, end_pos=-1):
+        search_result = re.compile(expression).search(text, start_pos, end_pos)
+        # fallback
+        if not search_result and start_pos > 0:
+            return ScraperService.__search_for_regex(expression, text, 0, len(text) - 1)
+        return ScraperService.__extract_result(search_result)
 
-	@staticmethod
-	def __parse_car(soup, url):
-		parsed_data = {CAR_KEY: url}
-		soup_text = soup.text.replace('\xa0', ' ')
-		# at first we search from the start of texts, then trying start-delta
-		data_assumed_start_position = 0
-		data_radius = 1000
+    @staticmethod
+    def __parse_car(soup, url):
+        parsed_data = {CAR_KEY: url}
+        soup_text = soup.text.replace('\xa0', ' ')
+        # at first we search from the start of texts, then trying start-delta
+        data_assumed_start_position = 0
+        data_radius = 1000
 
-		# our kinda fix position
-		search_result = re.search('\d{1,4} ?kW', soup_text)
+        # our kinda fix position
+        search_result = re.search('\d{1,4} ?kW', soup_text)
 
-		# print('Search result for power ' + str(search_result))
-		power = ScraperService.__extract_result(search_result)
-		parsed_data[POWER_KEY] = power
+        # print('Search result for power ' + str(search_result))
+        power = ScraperService.__extract_result(search_result)
+        parsed_data[POWER_KEY] = power
 
-		if search_result:
-			search_result_start = search_result.span()[0]
-			data_assumed_start_position = search_result_start - data_radius if search_result_start > data_radius else 0
-			data_assumed_end_position = search_result_start + data_radius
-			data_assumed_end_position = data_assumed_end_position if data_assumed_end_position < len(soup_text) else -1
+        if search_result:
+            search_result_start = search_result.span()[0]
+            data_assumed_start_position = search_result_start - data_radius if search_result_start > data_radius else 0
+            data_assumed_end_position = search_result_start + data_radius
+            data_assumed_end_position = data_assumed_end_position if data_assumed_end_position < len(soup_text) else -1
 
-		# numbers delimited by dot, space or coma, find the first
-		parsed_data[SPEEDOMETER_KEY] = ScraperService.__search_for_regex('((\d{1,3}[., ]?){1,3})([kK][mM])|(miles)',
-												 soup_text, data_assumed_start_position, data_assumed_end_position)
+        # numbers delimited by dot, space or coma, find the first
+        parsed_data[SPEEDOMETER_KEY] = ScraperService.__search_for_regex('((\d{1,3}[., ]?){1,3})([kK][mM])|(miles)',
+                                                                         soup_text, data_assumed_start_position,
+                                                                         data_assumed_end_position)
 
-		# all yyyy/mm and mm/yyyy formats are accepted (days also)
-		# note: parser must check values
-		date_string = ScraperService.__search_for_regex('(\d{4}([/.]\d{1,2}){1,2})|(\d{1,2}([/.]\d{1,2})?[/.]\d{4})',
-												 soup_text, data_assumed_start_position, data_assumed_end_position)
-		parsed_data[AGE_KEY] = date_string if ScraperService.__is_date_valid(date_string) else ''
+        # all yyyy/mm and mm/yyyy formats are accepted (days also)
+        # note: parser must check values
+        date_string = ScraperService.__search_for_regex('(\d{4}([/.]\d{1,2}){1,2})|(\d{1,2}([/.]\d{1,2})?[/.]\d{4})',
+                                                        soup_text, data_assumed_start_position,
+                                                        data_assumed_end_position)
+        parsed_data[AGE_KEY] = date_string if ScraperService.__is_date_valid(date_string) else ''
 
-		price_text = ScraperService.__search_for_regex(
-			'((€|£|(Ft)|(HUF)) ?(\d{1,3}[., ]?){2,3})|((\d{1,3}[., ]?){2,3}(€|£|(Ft)|(HUF)))', soup_text,
-			data_assumed_start_position, data_assumed_end_position)
-		parsed_data[PRICE_KEY] = ValueParser.get_first_number(price_text)
-		parsed_data[CURRENCY_KEY] = ValueParser.get_currency_iso_symbol(price_text)
-		return parsed_data
+        price_text = ScraperService.__search_for_regex(
+            '((€|£|(Ft)|(HUF)) ?(\d{1,3}[., ]?){2,3})|((\d{1,3}[., ]?){2,3}(€|£|(Ft)|(HUF)))', soup_text,
+            data_assumed_start_position, data_assumed_end_position)
+        parsed_data[PRICE_KEY] = str(ValueParser.get_first_number(price_text))
+        parsed_data[CURRENCY_KEY] = ValueParser.get_currency_iso_symbol(price_text)
+        return parsed_data
 
-	@staticmethod
-	def __is_date_valid(date_string):
-		"""
-		Validates the date string
-		Valid values are YYYY/MM[/*] and [DD/]MM/YYYY
-		Delimiter can be . and /
-		:rtype Boolean
-		:return True if string is a date False otherwise
-		"""
-		if date_string == '':
-			return False
-		date_string = date_string.replace('.', '/')
-		prod_date_arr = list(map(int, date_string.split('/')))
-		if len(prod_date_arr) < 2:
-			return False
-		elif len(prod_date_arr) >= 2:
-			# 2005/03
-			if is_string_year(prod_date_arr[0]):
-				return is_string_month(prod_date_arr[1])
-			# 03/2005
-			elif is_string_year(prod_date_arr[1]):
-				return is_string_month(prod_date_arr[0])
-			# 25/03/2005
-			else:
-				return len(prod_date_arr) > 2 and is_string_year(prod_date_arr[2]) and is_string_month(prod_date_arr[1])
+    @staticmethod
+    def __is_date_valid(date_string):
+        """
+        Validates the date string
+        Valid values are YYYY/MM[/*] and [DD/]MM/YYYY
+        Delimiter can be . and /
+        :rtype Boolean
+        :return True if string is a date False otherwise
+        """
+        if date_string == '':
+            return False
+        date_string = date_string.replace('.', '/')
+        prod_date_arr = list(map(int, date_string.split('/')))
+        if len(prod_date_arr) < 2:
+            return False
+        elif len(prod_date_arr) >= 2:
+            # 2005/03
+            if is_string_year(prod_date_arr[0]):
+                return is_string_month(prod_date_arr[1])
+            # 03/2005
+            elif is_string_year(prod_date_arr[1]):
+                return is_string_month(prod_date_arr[0])
+            # 25/03/2005
+            else:
+                return len(prod_date_arr) > 2 and is_string_year(prod_date_arr[2]) and is_string_month(prod_date_arr[1])
 
-	def get_car_data(self):
-		"""
-		Gets car data from the urls the service was initialized with, or uses prefetched html data
-		:rtype: dict
-		:return: car data in dictionary. For keys see CAR_FEATURE_KEY_MAP's values
-		"""
-		cars = []
-		headers = {'User-Agent': 'Chrome/60.0.3112.113'}
-		for car_url in self.car_urls:
-			if car_url not in self.htmls:
-				response = requests.get(car_url, headers=headers)
-				content = str(response.content, encoding='utf-8')
-			else:
-				content = self.htmls[car_url]
-			content = content.replace('\\xa0', ' ')
-			car_soup = BeautifulSoup(content, 'lxml')
-			cars.append(ScraperService.__parse_car(car_soup, car_url))
-		return cars
+    def get_car_data(self):
+        """
+        Gets car data from the urls the service was initialized with, or uses prefetched html data
+        :rtype: dict
+        :return: car data in dictionary. For keys see CAR_FEATURE_KEY_MAP's values
+        """
+        cars = []
+        headers = {'User-Agent': 'Chrome/60.0.3112.113'}
+        for car_url in self.car_urls:
+            if car_url not in self.htmls:
+                response = requests.get(car_url, headers=headers)
+                content = str(response.content, encoding='utf-8')
+            else:
+                content = self.htmls[car_url]
+            content = content.replace('\\xa0', ' ')
+            car_soup = BeautifulSoup(content, 'lxml')
+            cars.append(ScraperService.__parse_car(car_soup, car_url))
+        return cars
 
 
 class ScraperServiceFactory:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	@staticmethod
-	def get_for_lists(url_list, content_list) -> ScraperService:
-		"""
-		Constructs a new Scraper sercice from two lists
-		:param url_list: list of urls
-		:param content_list: list of html pages in same order as in url list
-		:return: new ScraperService instance with the given content
-		"""
-		htmls = {url_list[i]: content_list[i] for i in range(len(url_list))}
-		return ScraperService(url_list, htmls)
+    @staticmethod
+    def get_for_lists(url_list, content_list) -> ScraperService:
+        """
+        Constructs a new Scraper sercice from two lists
+        :param url_list: list of urls
+        :param content_list: list of html pages in same order as in url list
+        :return: new ScraperService instance with the given content
+        """
+        htmls = {url_list[i]: content_list[i] for i in range(len(url_list))}
+        return ScraperService(url_list, htmls)
 
-	@staticmethod
-	def get_for_dict(htmls: dict) -> ScraperService:
-		"""
-		Convenience method for ScraperService creation
-		:param htmls: dictionary where the keys are urls and the values are the html contents of the urls
-		:return: a ScraperService of the given data
-		"""
-		return ScraperService([*htmls], htmls)
+    @staticmethod
+    def get_for_dict(htmls: dict) -> ScraperService:
+        """
+        Convenience method for ScraperService creation
+        :param htmls: dictionary where the keys are urls and the values are the html contents of the urls
+        :return: a ScraperService of the given data
+        """
+        return ScraperService([*htmls], htmls)
 
-	@staticmethod
-	def get_for_list_and_dict(url_list: list, htmls: dict) -> ScraperService:
-		"""
-		Convenience method for ScraperService creation. Urls might be different from urls in htmls' keys
-		:param url_list: list of urls
-		:param htmls: dictionary where the keys are urls and the values are the html contents of the urls
-		:return: a ScraperService of the given data
-		"""
-		complete_url_list = list({*htmls} | set(url_list))
-		return ScraperService(complete_url_list, htmls)
+    @staticmethod
+    def get_for_list_and_dict(url_list: list, htmls: dict) -> ScraperService:
+        """
+        Convenience method for ScraperService creation. Urls might be different from urls in htmls' keys
+        :param url_list: list of urls
+        :param htmls: dictionary where the keys are urls and the values are the html contents of the urls
+        :return: a ScraperService of the given data
+        """
+        complete_url_list = list({*htmls} | set(url_list))
+        return ScraperService(complete_url_list, htmls)
 
 
 def main():
-	parser = argparse.ArgumentParser(description='Provide car URLs for scraping.')
-	parser.add_argument('car_urls', nargs='+', metavar='URLs', help='At least one car URL is required')
-	namespace = parser.parse_args()
-	car_urls = namespace.car_urls
+    parser = argparse.ArgumentParser(description='Provide car URLs for scraping.')
+    parser.add_argument('car_urls', nargs='+', metavar='URLs', help='At least one car URL is required')
+    namespace = parser.parse_args()
+    car_urls = namespace.car_urls
 
-	scraper = ScraperService(car_urls)
-	data = scraper.get_car_data()
-	CarComparator.compare_cars(data)
-	for car in data:
-		print(car['CarUri'])
-		print(car['worth'])
+    scraper = ScraperService(car_urls)
+    data = scraper.get_car_data()
+    CarComparator.compare_cars(data)
+    for car in data:
+        print(car['CarUri'])
+        print(car['worth'])
 
-	if __name__ == "__main__":
-		main()
+    if __name__ == "__main__":
+        main()
--- tests/test_value_parser.py
+++ tests/test_value_parser.py
@@ -3,116 +3,117 @@
 import unittest
 from datetime import date
 
-from skyscraper.utils.constants import POWER_KEY, TRUNK_KEY, MASS_KEY, PRICE_KEY, AGE_KEY
+from skyscraper.utils.constants import POWER_KEY, TRUNK_KEY, MASS_KEY, PRICE_KEY, AGE_KEY, CURRENCY_KEY
 from skyscraper.utils.constants import SPEEDOMETER_KEY, CAR_KEY, CONDITION_KEY
 from skyscraper.utils.value_parser import ValueParser
 
 
 class TestBasicPaths(unittest.TestCase):
-	default_input = {}
-	value_parser = ValueParser(default_input)
+    default_input = {}
+    value_parser = ValueParser(default_input)
 
-	@staticmethod
-	def date_to_age(years, months):
-		today = date.today()
-		month_diff = today.month - months
-		if month_diff <= 0:
-			years += 1
-			months = 12 + month_diff
-		else:
-			months = month_diff
-		return date(today.year - years, months, 1).strftime("%Y/%m")
+    @staticmethod
+    def date_to_age(years, months):
+        today = date.today()
+        month_diff = today.month - months
+        if month_diff <= 0:
+            years += 1
+            months = 12 + month_diff
+        else:
+            months = month_diff
+        return date(today.year - years, months, 1).strftime("%Y/%m")
 
-	def setUp(self):
-		self.default_input = {
-				CAR_KEY: 'http://hasznaltauto.hu/auto',
-				CONDITION_KEY: 'Újszerű',
-				SPEEDOMETER_KEY: '0 km',
-				AGE_KEY: date.today().strftime("%Y/%m")
-			}
-		self.value_parser = ValueParser(self.default_input)
+    def setUp(self):
+        self.default_input = {
+            CAR_KEY: 'http://hasznaltauto.hu/auto',
+            CONDITION_KEY: 'Újszerű',
+            SPEEDOMETER_KEY: '0 km',
+            AGE_KEY: date.today().strftime("%Y/%m")
+        }
+        self.value_parser = ValueParser(self.default_input)
 
-	def test_power_worth(self):
-		car = self.default_input
-		car[POWER_KEY] = '42 kW'
+    def test_power_worth(self):
+        car = self.default_input
+        car[POWER_KEY] = '42 kW'
 
-		power_worth = self.value_parser.get_power_value()
+        power_worth = self.value_parser.get_power_value()
 
-		self.assertEqual(power_worth, 3)
+        self.assertEqual(power_worth, 3)
 
-	def test_condition_worth(self):
-		condition_worth = self.value_parser.get_condition_value()
-		self.assertEqual(condition_worth, 0)
+    def test_condition_worth(self):
+        condition_worth = self.value_parser.get_condition_value()
+        self.assertEqual(condition_worth, 0)
 
-		car = self.default_input
-		car[CONDITION_KEY] = ''
-		condition_worth = self.value_parser.get_condition_value()
-		self.assertEqual(condition_worth, -20)
+        car = self.default_input
+        car[CONDITION_KEY] = ''
+        condition_worth = self.value_parser.get_condition_value()
+        self.assertEqual(condition_worth, -20)
 
-	def test_trunk_worth(self):
-		car = self.default_input
-		car[TRUNK_KEY] = '290 l'
+    def test_trunk_worth(self):
+        car = self.default_input
+        car[TRUNK_KEY] = '290 l'
 
-		trunk_worth = self.value_parser.get_trunk_value()
+        trunk_worth = self.value_parser.get_trunk_value()
 
-		self.assertEqual(trunk_worth, 2)
+        self.assertEqual(trunk_worth, 2)
 
-	def test_mass_worth(self):
-		car = self.default_input
-		car[MASS_KEY] = '1600 kg'
+    def test_mass_worth(self):
+        car = self.default_input
+        car[MASS_KEY] = '1600 kg'
 
-		mass_worth = self.value_parser.get_mass_value()
+        mass_worth = self.value_parser.get_mass_value()
 
-		self.assertEqual(mass_worth, 3)
+        self.assertEqual(mass_worth, 3)
 
-	def test_speedometer_worth(self):
-		self.assert_speedo('0km', 0)
-		self.assert_speedo('0 km', 0)
-		self.assert_speedo('92,000 km', -9)
-		self.assert_speedo('140 000 km', -12)
-		self.assert_speedo('240 000 km', -16)
+    def test_speedometer_worth(self):
+        self.assert_speedo('0km', 0)
+        self.assert_speedo('0 km', 0)
+        self.assert_speedo('92,000 km', -9)
+        self.assert_speedo('140 000 km', -12)
+        self.assert_speedo('240 000 km', -16)
 
-	def test_price_worth(self):
-		car = self.default_input
+    def test_price_worth(self):
+        car = self.default_input
 
-		#no power, no price
-		price_worth = self.value_parser.get_price_value()
-		self.assertEqual(price_worth, 0)
+        # no power, no price
+        price_worth = self.value_parser.get_price_value()
+        self.assertEqual(price_worth, 0)
 
-		#no power
-		car[PRICE_KEY] = '6.000.000 Ft'
-		price_worth = self.value_parser.get_price_value()
-		self.assertEqual(price_worth, 0)
+        # no power
+        car[PRICE_KEY] = '6000000'
+        price_worth = self.value_parser.get_price_value()
+        self.assertEqual(price_worth, 0)
 
-		#no price
-		del car[PRICE_KEY]
-		car[POWER_KEY] = '100 kW'
-		price_worth = self.value_parser.get_price_value()
-		self.assertEqual(price_worth, 0)
+        # no price
+        del car[PRICE_KEY]
+        car[POWER_KEY] = '100 kW'
+        price_worth = self.value_parser.get_price_value()
+        self.assertEqual(price_worth, 0)
 
-		#price and power
-		car[PRICE_KEY] = '€ 26.535'
-		price_worth = self.value_parser.get_price_value()
-		self.assertEqual(price_worth, 10)
+        # price and power
+        car[PRICE_KEY] = '26535'
+        car[CURRENCY_KEY] = 'EUR'
+        price_worth = self.value_parser.get_price_value()
+        self.assertEqual(price_worth, 10)
 
-	def test_age_worth(self):
-		self.assert_age(0, 3, -3)
-		self.assert_age(1, 0, -10)
-		self.assert_age(10, 0, -23)
-		self.assert_age(30, 0, -25)
-		self.assert_age(50, 0, -1)
+    def test_age_worth(self):
+        self.assert_age(0, 3, -3)
+        self.assert_age(1, 0, -10)
+        self.assert_age(10, 0, -31)
+        self.assert_age(30, 0, -35)
+        self.assert_age(50, 0, 37)
 
-	'''ASSERTIONS'''
+    '''ASSERTIONS'''
 
-	def assert_speedo(self, kilometers, expected):
-		car = self.default_input
+    def assert_speedo(self, kilometers, expected):
+        car = self.default_input
 
-		car[SPEEDOMETER_KEY] = kilometers
-		speedo_worth = self.value_parser.get_speedometer_value()
-		self.assertEqual(speedo_worth, expected)
+        car[SPEEDOMETER_KEY] = kilometers
+        speedo_worth = self.value_parser.get_speedometer_value()
+        self.assertEqual(speedo_worth, expected)
 
-	def assert_age(self, years, months, expected):
-		car = self.default_input
-		car[AGE_KEY] = TestBasicPaths.date_to_age(years, months)
-		age_worth = self.value_parser.get_age_value()
-		self.assertEquals(age_worth, expected)
+    def assert_age(self, years, months, expected):
+        car = self.default_input
+        car[AGE_KEY] = TestBasicPaths.date_to_age(years, months)
+        age_worth = self.value_parser.get_age_value()
+        self.assertEqual(expected, age_worth)
